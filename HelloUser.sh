c8d4db2a7bfbb03a4fd95a876fb51028d8da4054
None <none@gmail.com>
2020-02-29 11:17:28 +0530

    First_Commit

---
+++ HelloUser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+# @Purpose : Say Hello To User
+# @Author : Akshay D. Barve
+# @Version : 18.04.3 lts
+# @Since : 27-02-2020 / Thursday
+
+#Constant
+user=Akshay
+
+# Hello To  Declared User
+echo Local user is $user
+echo Hello $user...
+
+# Hello To Root User
+echo Root user is $USER
+echo Hello $USER...
