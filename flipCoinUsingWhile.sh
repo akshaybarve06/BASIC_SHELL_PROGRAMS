d867752d7457d6d511c5f715a4fb8fc67683d0bd
Admin <none@gmail.com>
2020-03-02 18:39:41 +0530

    Flip_Coin_Print_Output

---
+++ flipCoinUsingWhile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+# @Purpose : Flip coin till either head or tails win 11 times
+# @Author : Akshay Dhananjay Barve
+# @Version : 18.04.3 lts
+# @Since : 02-03-2020 / Monday
+
+headcount=0
+tailcount=0
+
+while [[ $headcount -ne 11 && $tailcount -ne 11 ]]
+do
+	coinoutput=$((RANDOM % 2))
+	if [[ $coinoutput -eq 1 ]]
+	then
+		((headcount++))
+	else
+		((tailcount++))
+	fi
+
+	if [[ $headcount -eq 11 ]]
+   then 
+      echo "Head count 11..Head Won"
+		exit
+   elif [[ $tailcount -eq 11 ]]
+   then
+      echo "Tail Count 11..Tail Won"
+		exit
+   fi
+done
