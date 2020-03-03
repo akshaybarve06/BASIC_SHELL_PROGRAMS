d10c8b8f3ca9d30bfacb8cf524ea8ac6b7f541a5
Admin <none@gmail.com>
2020-03-03 14:30:48 +0530

    Second_Large_Small_Without_Sort

---
+++ secondLargestSmallest.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -x
+
+# @Purpose : Generate 10 - 3 Digit Number Find second Smallest and second Largest of them
+# @Author : Akshay Dhananjay Barve
+# @Version : 18.04.3 lts
+# @Since : 03-03-2020 / Tuesday
+
+declare -a array
+
+function numberGenerate()
+{
+#	declare -a array
+	for (( i=0; i<10; i++ ))
+	do
+		number=$(((RANDOM %900)+100))
+		array[i]=$number
+	done
+	echo
+	echo Generated Array.. ${array[@]}
+	echo
+}
+
+function secondLargest()
+{
+	largest=$1
+	secondLargest=$1
+	for m in $*
+	do
+		if [[ $m -gt $largest ]]
+		then
+			secondLargest=$largest
+			largest=$m
+		elif [[ $m -gt secondLargest ]]
+		then
+			secondLargest=$m
+		fi
+	done
+	echo Largest Element of Array is..$largest
+	echo Second Largest Element of Array is.. $secondLargest
+	echo
+}
+
+function secondSmallest()
+{
+   smallest=$1
+   secondSmallest=$1
+   for m in $*
+   do
+      if [[ $m -lt $smallest ]]
+      then
+         secondSmallest=$smallest
+         smallest=$m
+      elif [[ $m -lt secondSmallest ]]
+      then
+         secondSmallest=$m
+      fi
+   done
+   echo Smallest Element of Array is..$smallest
+   echo Second Smallest Element of Array Is..$secondSmallest
+	echo
+}
+
+numberGenerate
+secondLargest ${array[@]}
+secondSmallest ${array[@]}
