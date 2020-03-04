02c7e90971ed88014b23bcf9dd90f432a5f24600
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-04 10:15:33 +0530

    Update UC7empWageWorkHrs.sh

--- UC7empWageWorkHrs.sh
+++ UC7empWageWorkHrs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-# @Purpose : Print Number In Words Using Case
+# @Purpose : Employee Use Case 7
 # @Author : Akshay Dhananjay Barve
 # @Version : 18.04.3 lts
 # @Since : 03-03-2020 / Tuesday
